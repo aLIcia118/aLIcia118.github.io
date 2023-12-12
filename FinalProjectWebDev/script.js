957f2af9d6acf77a83d28e2c2839c1df091f2591
alicia <144707794+aLIcia118@users.noreply.github.com>
2023-12-12 17:13:11 -0500

    Update script.js

--- FinalProjectWebDev/script.js
+++ FinalProjectWebDev/script.js
@@ -44,6 +44,12 @@ function saveImage() {
   if (randomImageSrc && randomCaption) {
     const savedImagesContainer = document.getElementById('saved-images');
 
+    if (savedImageCount % 8 === 0) {
+      // Create a new line before adding the next set of saved images
+      const lineBreak = document.createElement('br');
+      savedImagesContainer.appendChild(lineBreak);
+    }
+
     const savedImageBox = document.createElement('div');
     savedImageBox.classList.add('saved-image-box');
 
@@ -52,8 +58,19 @@ function saveImage() {
     savedImage.src = randomImageSrc;
     savedImage.alt = randomCaption;
 
+    // Show caption on hover
+    savedImage.addEventListener('mouseover', function () {
+      document.getElementById('hover-caption').textContent = randomCaption;
+    });
+
+    // Clear caption on mouseout
+    savedImage.addEventListener('mouseout', function () {
+      document.getElementById('hover-caption').textContent = '';
+    });
+
     savedImageBox.appendChild(savedImage);
     savedImagesContainer.appendChild(savedImageBox);
+
+    savedImageCount++;
   }
 }
-
