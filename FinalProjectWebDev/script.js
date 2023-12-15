046dbc0e090d29c069cfd099421063d21426bbe4
alicia <144707794+aLIcia118@users.noreply.github.com>
2023-12-15 16:21:53 -0500

    Update script.js

--- FinalProjectWebDev/script.js
+++ FinalProjectWebDev/script.js
@@ -12,34 +12,26 @@ function showConfirmation() {
   const savedImages = savedImagesContainer.querySelectorAll('.saved-image');
 
   if (savedImages.length > 0) {
-    // Display a custom confirmation modal
-    const confirmationModal = document.createElement('div');
-    confirmationModal.classList.add('confirmation-modal');
+    // Display a confirmation dialog
+    const confirmation = confirm("Oh no! Your collection will not be saved. Do you want to continue?");
 
-    const message = document.createElement('p');
-    message.textContent = "Oh no! Your collection will not be saved. Do you want to continue?";
-    confirmationModal.appendChild(message);
-
-    const confirmButton = document.createElement('button');
-    confirmButton.textContent = 'OK';
-    confirmButton.addEventListener('click', function () {
-      window.location.href = "https://www.sonnyangel.com/en/"; 
-    });
-    confirmationModal.appendChild(confirmButton);
-
-    const cancelButton = document.createElement('button');
-    cancelButton.textContent = 'Cancel';
-    cancelButton.addEventListener('click', function () {
-      document.body.removeChild(confirmationModal);
-    });
-    confirmationModal.appendChild(cancelButton);
-
-    document.body.appendChild(confirmationModal);
+    // If the user clicks "OK", navigate to the link
+    if (confirmation) {
+      window.location.href = "https://www.sonnyangel.com/en/";
+    } else {
+      // If the user clicks "Cancel", remove the confirmation message
+      // You may need to customize this part based on your HTML structure
+      const confirmationMessage = document.querySelector('.confirmation-message');
+      if (confirmationMessage) {
+        confirmationMessage.remove();
+      }
+    }
   } else {
     // No saved images, no need for confirmation
-    window.location.href = "https://www.sonnyangel.com/en/"; 
+    window.location.href = "https://www.sonnyangel.com/en/";
   }
 }
+
 
 
 
